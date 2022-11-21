260e20ec51c5f4af0a46f775b5c3d1d1acccfbf9
Kasl0 <slonieckacper@gmail.com>
2022-11-21 18:10:37 +0100

    Lab3

---
+++ haskell-lab3/ex1.hs
@@ -0,0 +1,17 @@
+--let f1 = \x -> x - 2
+--let f2 = \x y -> sqrt(x^2 + y^2)
+--let f3 = \x y z -> sqrt(x^2 + y^2 + z^2)
+
+--let f4 = \x -> 2*x
+--let f5 = \x -> x*2
+--let f6 = \x -> 2^x
+--let f7 = \x -> x^2
+--let f8 = \x -> 2/x
+--let f9 = \x -> x/3
+--let f10 = \x -> 4-x
+
+--let f11 = \x -> sqrt(x)
+--let f12 = \x -> abs(x)
+--let f13 = \x -> log(x)
+--let f14 = \x -> id(x)
+--let f15 = \x -> const(x)
